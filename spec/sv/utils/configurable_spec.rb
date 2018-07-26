2801f128f6c9f3d7fa052b7c876908243a7ff2e4
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-26 15:53:07 +0200

    utils/configurable (spec) trivial method tests

--- spec/sv/utils/configurable_spec.rb
+++ spec/sv/utils/configurable_spec.rb
@@ -2,4 +2,26 @@
 
 describe Sv::Utils::Configurable, :'utils/configurable' do
   it { expect(described_class).to be_const_defined(:Command) }
+
+  # class methods
+  it do
+    expect(described_class).to respond_to(:new).with(1).arguments
+    expect(described_class).to respond_to(:new).with(2).arguments
+  end
+end
+
+describe Sv::Utils::Configurable, :'utils/configurable' do
+  let(:subject) { described_class.new({}) }
+
+  # instance methods
+  it do
+    expect(subject).to respond_to(:params).with(0).arguments
+    expect(subject).to respond_to(:root_key).with(0).arguments
+  end
+
+  # attribute readers
+  it do
+    expect(subject).to respond_to(:config).with(0).arguments
+    expect(subject).to respond_to(:options).with(0).arguments
+  end
 end
