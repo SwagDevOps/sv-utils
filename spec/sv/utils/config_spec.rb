fe1ec51a51eace6a9d068fd666cf7344dcce828d
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-03 18:50:09 +0200

    utils/config_spec (spec) examples added

--- spec/sv/utils/config_spec.rb
+++ spec/sv/utils/config_spec.rb
@@ -58,11 +58,48 @@ describe Sv::Utils::Config, :'utils/config' do
 end
 
 describe Sv::Utils::Config, :'utils/config' do
-  let(:sample) { sham!(:configs).samples.fetch('invalid') }
+  let(:sample) { sham!(:configs).samples.fetch('invalid_syntax') }
 
   context '#new' do
     it 'should raise' do
       expect { described_class.new(sample) }.to raise_error(Psych::SyntaxError)
     end
+  end
+end
+
+describe Sv::Utils::Config, :'utils/config' do
+  let(:sample) { sham!(:configs).samples.fetch('invalid_logic') }
+
+  context '#new' do
+    it 'should raise' do
+      expect { described_class.new(sample) }.to raise_error(Errno::EINVAL)
+    end
+  end
+end
+
+describe Sv::Utils::Config, :'utils/config', :wip do
+  let(:sample) { sham!(:configs).samples.fetch('override') }
+  let(:subject) { described_class.new(sample) }
+
+  context '#["loggerd"]["user"]' do
+    it { expect(subject['loggerd']['user']).to eq('svlog') }
+  end
+
+  context '#["loggerd"]["group"]' do
+    it { expect(subject['loggerd']['group']).to eq('svlog') }
+  end
+
+  context '#["service"].fetch("command")' do
+    it { expect(subject['service'].fetch('command')).to be(nil) }
+  end
+
+  context '#["control"]["futils"]' do
+    it { expect(subject['control']['futils']).to eq('dry_run') }
+  end
+
+  context '#["control"]["paths"]' do
+    let(:paths) { ['/etc/service', '/var/run/sv'] }
+
+    it { expect(subject['control']['paths']).to eq(paths) }
   end
 end
