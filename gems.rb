23ec116d87b21343e580bb2facf095ea35b0a580
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-07 10:54:19 +0200

    minor changes in gems

--- gems.rb
+++ gems.rb
@@ -14,21 +14,17 @@ group :development do
   gem 'kamaze-project', '~> 1.0', '>= 1.0.3'
   gem 'listen', '~> 3.1'
   gem 'rubocop', '~> 0.58'
+  # repl ---------------------------------
+  gem 'interesting_methods', '~> 0.1'
+  gem 'pry', '~> 0.11'
+  gem 'pry-coolline', '~> 0.2'
+  # doc ----------------------------------
+  gem 'github-markup', '~> 2.0'
+  gem 'redcarpet', '~> 3.4'
+  gem 'yard', '~> 0.9'
+end
 
-  group :repl do
-    gem 'interesting_methods', '~> 0.1'
-    gem 'pry', '~> 0.11'
-    gem 'pry-coolline', '~> 0.2'
-  end
-
-  group :doc do
-    gem 'github-markup', '~> 2.0'
-    gem 'redcarpet', '~> 3.4'
-    gem 'yard', '~> 0.9'
-  end
-
-  group :test do
-    gem 'rspec', '~> 3.8'
-    gem 'sham', '~> 2.0'
-  end
+group :test do
+  gem 'rspec', '~> 3.8'
+  gem 'sham', '~> 2.0'
 end
