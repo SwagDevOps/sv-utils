5693b35debefef12e72b0db3221cdbe8f32cbd06
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-03-28 08:29:18 +0100

    shell_spec (spec) class methods spec added

--- spec/sv/utils/shell_spec.rb
+++ spec/sv/utils/shell_spec.rb
@@ -6,3 +6,11 @@ describe Sv::Utils::Shell, :'utils/shell' do
   it { expect(described_class).to be_const_defined(:Error) }
   it { expect(described_class).to be_const_defined(:ExitStatusError) }
 end
+
+# class methods
+describe Sv::Utils::Shell, :'utils/shell' do
+  it do
+    expect(described_class).to respond_to(:new).with(1).arguments
+    expect(described_class).to respond_to(:new).with(2).arguments
+  end
+end
