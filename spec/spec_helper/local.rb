4a2f2504c637359b9abfdd5e04af325b4df366f0
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-07 14:36:18 +0200

    spec_helper/local (spec) silence_stream method introduced

--- spec/spec_helper/local.rb
+++ spec/spec_helper/local.rb
@@ -12,4 +12,24 @@ module Local
   def sham!(name)
     FactoryStruct.sham!(name)
   end
+
+  # Silences any stream for the duration of the block.
+  #
+  # @see https://apidock.com/rails/Kernel/silence_stream
+  def silence_stream(stream) # rubocop:disable Metrics/MethodLength
+    @silence_stream_mutex ||= Mutex.new
+
+    @silence_stream_mutex.synchronize do
+      begin
+        old_stream = stream.dup
+        (RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ? 'NUL:' : '/dev/null')
+          .tap { |stream_null| stream.reopen(stream_null) }
+        stream.sync = true
+        yield
+      ensure
+        stream.reopen(old_stream)
+        old_stream.close
+      end
+    end
+  end
 end
