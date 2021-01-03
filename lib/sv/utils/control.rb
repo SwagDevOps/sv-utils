9bc7f3f2bf6fb07aa22dedcdb4b81184d81f7a8b
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-03 18:34:17 +0100

    control (lib) refactor, almost syntactic changes

--- lib/sv/utils/control.rb
+++ lib/sv/utils/control.rb
@@ -9,67 +9,65 @@
 require_relative '../utils'
 autoload :Pathname, 'pathname'
 
-module Sv::Utils
-  # Control do enable disable services.
-  class Control < Configurable
-    autoload :Actionable, "#{__dir__}/control/actionable"
+# Control do enable disable services.
+class Sv::Utils::Control < Sv::Utils::Configurable
+  autoload :Actionable, "#{__dir__}/control/actionable"
 
-    # Enable service by given name.
-    #
-    # @param [String|Symbol] name
-    # @param [Hash{Symbol => Object}] params
-    def enable(name, params = {})
-      call(:enable, [name].push(params))
+  # Enable service by given name.
+  #
+  # @param [String|Symbol] name
+  # @param [Hash{Symbol => Object}] params
+  def enable(name, params = {})
+    call(:enable, [name].push(params))
+  end
+
+  # Disable service by given name.
+  #
+  # @param [String|Symbol] name
+  # @param [Hash{Symbol => Object}] params
+  def disable(name, params = {})
+    call(:enable, [name].push(params))
+  end
+
+  # @param [String|Symbol] action
+  def call(action, *args)
+    params = {}
+    if args.last.is_a?(Hash)
+      params = args.last
+      args = args[0...-1]
     end
 
-    # Disable service by given name.
-    #
-    # @param [String|Symbol] name
-    # @param [Hash{Symbol => Object}] params
-    def disable(name, params = {})
-      call(:enable, [name].push(params))
-    end
+    # noinspection RubyNilAnalysis
+    actions.fetch(action.to_sym).call(*args.push(self.params.merge(params)))
+  end
 
-    # @param [String|Symbol] action
-    def call(action, *args)
-      params = {}
-      if args.last.is_a?(Hash)
-        params = args.last
-        args = args[0...-1]
-      end
-
-      actions.fetch(action.to_sym)
-             .call(*args.push(self.params.merge(params)))
-    end
-
-    class << self
-      # @return [Array<Symbol>]
-      def actions
-        Actionable.actions.keys
-      end
-    end
-
-    # @raise [KeyError]
-    # @return [Hash{Symbol => Object}]
-    #
-    # @todo Improve paths retrieval
-    def params
-      {
-        futils: config['futils'],
-        paths: [
-          Pathname.new(config['paths'].to_a.fetch(0)),
-          lambda do
-            # prioritize SVDIR over configuration
-            (ENV['SVDIR'] || config['paths'].to_a[1] || ENV.fetch('SVDIR'))
-              .tap { |dir| return Pathname.new(dir) }
-          end.call
-        ].freeze,
-      }
-    end
-
-    # @return [Hash{Symbol => Method}]
+  class << self
+    # @return [Array<Symbol>]
     def actions
-      Actionable.actions
+      Actionable.actions.keys
     end
+  end
+
+  # @raise [KeyError]
+  # @return [Hash{Symbol => Object}]
+  #
+  # @todo Improve paths retrieval
+  def params
+    {
+      futils: config['futils'],
+      paths: [
+        Pathname.new(config['paths'].to_a.fetch(0)),
+        lambda do
+          # prioritize SVDIR over configuration
+          (ENV['SVDIR'] || config['paths'].to_a[1] || ENV.fetch('SVDIR'))
+            .tap { |dir| return Pathname.new(dir) }
+        end.call
+      ].freeze,
+    }
+  end
+
+  # @return [Hash{Symbol => Method}]
+  def actions
+    Actionable.actions
   end
 end
