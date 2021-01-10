318d51c782416da2c3188231ba1983dd76abbd26
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-10 20:26:29 +0100

    update license headers + version license_header

--- lib/sv/utils/config.rb
+++ lib/sv/utils/config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# Copyright (C) 3017-2021 Dimitri Arrigoni <dimitri@arrigoni.me>
 # License GPLv3+: GNU GPL version 3 or later
 # <http://www.gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
@@ -23,6 +23,7 @@ class Sv::Utils::Config < Hash
 
     load_file(Pathname.new(__dir__).join('config.yml'))
     return if file.nil? and !self.file&.readable?
+
     load_file(self.file)
   end
 
